1d1e37698e2fd6768d73deac35d9c8dc615c6962
apoorvaravi <apoorva.ravi@gmail.com>
2022-12-20 20:56:01 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,42 +5,24 @@ import './style.css';
 const element = document.getElementById('trappingRainwater');
 element.innerHTML = `<h1>TrappingRainwater</h1>`;
 element.onclick = () =>
-  (element.innerHTML = trappingRainWater([0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]));
+  (element.innerHTML = trappingRainWater([5, 0, 0, 0, 9]));
 
-// Two pointer techinques
-const trappingRainWater = function (array) {
-  let total = 0;
-  let maxLeft = 0,
-    maxRight = 0,
-    leftp = 0,
-    rightp = 0;
-  for (let i = 0; i < array.length; i++) {
-    leftp = i;
-    rightp = i;
-    while (leftp <= 0) {
-      maxLeft = Math.max(maxLeft, array[leftp]);
-      leftp--;
-    }
-    while (rightp > array.length) {
-      maxRight = Math.max(maxRight, array[rightp]);
-      rightp++;
-    }
-    console.log({ maxLeft, maxRight });
-    const cw = Math.min(maxLeft, maxRight) - array[i];
-    if(cw >= 0) {
-      total+=cw;
+const trappingRainWater = function (heights) {
+  let totalWater = 0;
+
+  for (let p = 0; p < heights.length; p++) {
+    let leftP = p,
+      rightP = p,
+      maxLeft = 0,
+      maxRight = 0;
+
+    maxLeft = Math.max(...heights.slice(0, p));
+    maxRight = Math.max(...heights.slice(p, heights.length));
+    const currentWater = Math.min(maxLeft, maxRight) - heights[p];
+    if (currentWater >= 0) {
+      totalWater += currentWater;
     }
   }
-  return total;
-};
 
-// const maximumWater = function (array) {
-//   let maxArea = 0;
-//   for (let i = 0; i < array.length; i++) {
-//     for (let j = i + 1; j < array.length; j++) {
-//       const area = Math.min(array[i], array[j]) * (j - i);
-//       maxArea = Math.max(maxArea, area);
-//     }
-//   }
-//   return maxArea;
-// };
+  return totalWater;
+};
