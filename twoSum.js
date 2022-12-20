55a1ba5dc1cf14dde570a1c231d1d96be2fb2bb5
apoorvaravi <apoorva.ravi@gmail.com>
2022-12-20 10:44:32 +0530

    maximumwater added

---
+++ twoSum.js
@@ -0,0 +1,37 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const twosumEle = document.getElementById('twoSum');
+twosumEle.innerHTML = `<h1>Two sum</h1>`;
+twosumEle.onclick = () => (twosumEle.innerHTML = twosum([1, 3, 7, 9, 2], 11));
+
+const twosum = function (array, target) {
+  let numMap = new Map();
+  for (let i = 0; i < array.length; i++) {
+    let ntf = array[i];
+    if (numMap.has(ntf)) {
+      return [numMap.get(ntf), i];
+    } else {
+      ntf = target - array[i];
+      numMap.set(ntf, i);
+    }
+  }
+
+  // Brute
+  // const twosum = function (array, target) {
+  // for (let i = 0; i < array.length; i++) {
+  //   for (let j = i + 1; j < array.length; j++) {
+  //     if (array[i] + array[j] == target) {
+  //       return [array[i], array[j]];
+  //     }
+  //   }
+  // }
+  // }
+
+  // const sum = array.reduce((total, amount) => {
+  //   console.log(total  + "::" +  amount);
+  //   return total + amount;
+  // }, 0);
+  // return sum;
+};
