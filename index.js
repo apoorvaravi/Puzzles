667a274944097476d47eee5befc7d4e0464d7dd8
apoorvaravi <apoorva.ravi@gmail.com>
2022-12-22 15:11:47 +0530

    2 files modified

--- index.js
+++ index.js
@@ -2,39 +2,30 @@
 import './style.css';
 
 // Write Javascript code!
-const element = document.getElementById('trappingRainwater');
-element.innerHTML = `<h1>TrappingRainwater</h1>`;
-element.onclick = () =>
-  (element.innerHTML = trappingRainWater([0, 1, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]));
+const element = document.getElementById('backspaceString');
+element.innerHTML = `<h1>BackspaceString</h1>`;
+element.onclick = () => (element.innerHTML = backspaceString('ab#z', 'ac#zbbb'));
 
-const trappingRainWater = function (heights) {
-  let maxleft = 0,
-    maxright = 0,
-    total = 0;
+const backspaceString = function (...args) {
+  const str1 = trunkHash(args[0]).toLocaleString();
+  const str2 = trunkHash(args[1]).toLocaleString();
+  if (str1.length !== str2.length) {
+    return false;
+  } else {
+    return str1 === str2;
+  }
+};
 
-  let left = 0,
-    right = heights.length - 1;
-
-  while (left < right) {
-    if (heights[left] <= heights[right]) {
-      if (heights[left] > maxleft) {
-        maxleft = heights[left];
-      } else {
-        total += maxleft - heights[left];
-      }
-      left++;
-    } else {
-      if (heights[right] > maxright) {
-        maxright = heights[right];
-      } else {
-        total += maxright - heights[right];
-      }
-      right--;
+function trunkHash(s) {
+  let sArray = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== '#') sArray.push(s[i]);
+    else {
+      sArray.pop(s[i - 1]);
     }
   }
-
-  return total;
-};
+  return sArray;
+}
 
 // one pointer brute force approach
 // const trappingRainWater = function (heights) {
