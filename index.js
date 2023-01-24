4ce6f658b91055902172b5422936f4814df8495e
apoorvaravi <apoorva.ravi@gmail.com>
2023-01-24 23:37:59 +0530

    2 files modified

--- index.js
+++ index.js
@@ -2,63 +2,31 @@
 import './style.css';
 
 // Write Javascript code!
-const element = document.getElementById('reverseBetweenList');
-element.innerHTML = `<h1>ReverseBetweenList</h1>`;
-element.onclick = () =>
-  (element.innerHTML = reverseBetweenList(linkedlist, 2, 4));
+const element = document.getElementById('orderinLexicalScope');
+element.innerHTML = `<h1>OrderinLexicalScope</h1>`;
+element.onclick = () => (element.innerHTML = orderinLexicalScope('apoorva', 2));
 
-class Node {
-  constructor(v, n = null) {
-    this.value = v;
-    this.next = n;
-  }
-}
-const result = [];
+function orderinLexicalScope(str, pass) {
+  let result = [...str];
 
-var printList = function (head) {
-  if (!head) {
-    return result;
+  let min;
+
+  for (let i = 0; i < pass; i++) {
+    for (let j = i + 1; j < str.length; j++) {
+      min = Math.min(result[i].charCodeAt(), result[j].charCodeAt());
+    }
+    if (result[i].charCodeAt() === min) {
+      continue;
+    }
+
+    if (result[i].charCodeAt() > min) {
+      const pos =
+        str.substring(i, str.length).indexOf(String.fromCharCode(min)) + 1;
+      let temp = result[i];
+      result[i] = result[pos];
+      result[pos] = temp;
+    }
   }
-  // console.table(head);
-  result.push(head.value);
-  printList(head.next);
+
   return result;
-};
-
-const linkedlist = [1, 2, 3, 4, 5].reduce((acc, val) => {
-  return new Node(val, acc);
-}, null);
-
-console.log([5, 4, 3, 2, 1]);
-var reverseBetweenList = function (head, m, n) {
-  let currentPos = 1,
-    currentNode = head;
-  let start = head;
-
-  while (currentPos < m) {
-    start = currentNode;
-
-    currentNode = currentNode.next;
-    currentPos++;
-  }
-
-  let newList = null,
-    tail = currentNode;
-  console.log(tail);
-  while (currentPos >= m && currentPos <= n) {
-    const next = currentNode.next;
-    currentNode.next = newList;
-    newList = currentNode;
-    currentNode = next;
-    currentPos++;
-  }
-
-  start.next = newList;
-  tail.next = currentNode;
-
-  if (m > 1) {
-    return printList(head);
-  } else {
-    return printList(newList);
-  }
-};
+}
